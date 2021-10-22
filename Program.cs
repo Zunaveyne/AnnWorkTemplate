7336c4508eb010028e4af407deb780d070c083be
Александр Щелочков <shchelochkov.alex@gmail.com>
2021-10-22 17:40:48 +0300

    Релиз

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © 2020 Zunaveyne. All rights reserved.
+// Contacts: <shchelochkov.alex@gmail.com>
+// License: CC BY-NC 4.0
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnnWorkTemplate
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
